7fd7fea0c69fc72ed35c7c9afad1a6dec30f3b9a
essennejaye <sjeffers02@gmail.com>
2020-06-27 21:55:25 -0700

    added data capture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
+var currentDateTime = moment().format("YYYY MM DD");
 var toDay = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(toDay);
+$(".save-btn").on("click", function () {
+    // var hourButton = event.target;
+    var hour = $(this).attr("hour");
+    var task = $(this)
+        .parent().prev();
+    var taskText = task.val();
+    var key = currentDateTime + " " + hour;
+    localStorage.setItem(key, taskText);
+});
+var loadPage = function () {
+    $(".save-btn").each(function () {
+        var hour = $(this).attr("hour");
+        var key = currentDateTime + " " + hour;
+        var taskText = localStorage.getItem(key);
+        if (taskText) {
+            var taskEl = $(this).parent().prev();
+            taskEl.val(taskText);
+        }
+    });
+}
+$(document).ready(function() {
+    loadPage();
+});
